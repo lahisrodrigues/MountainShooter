7283d096421b4fd19507c5a5cf1cb75e9c54c2a5
Lais Rodrigues <laispr96@gmail.com>
2026-03-01 16:02:26 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

---
+++ code/EntityFactory.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from code.Background import Background
+from code.Const import WIN_WIDTH
+
+
+class EntityFactory:
+
+    @staticmethod
+    def get_entity(entity_name: str, position =(0,0)):
+        match entity_name:
+            case 'level1Bg': # melhor fazer desta forma, pois não precisa fazer as cases para todos, economiza linhas e deixa de ficar repetitivo
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f'level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+
+
+
