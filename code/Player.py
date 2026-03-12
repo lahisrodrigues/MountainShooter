bcf9bf5882703e56b1e0d738a047bb19477def8a
Lais Rodrigues <laispr96@gmail.com>
2026-03-11 22:45:09 -0300

    Hit box Collisions HUD Score

--- code/Player.py
+++ code/Player.py
@@ -32,3 +32,4 @@ class Player(Entity):
             pressed_key = pygame.key.get_pressed()
             if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
                 return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+        return None
